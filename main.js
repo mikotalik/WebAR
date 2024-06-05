d6a625abdf28b4c49c626158bcd8244621e7c291
Miroslav Kotalik <mikotalik@seznam.cz>
2024-06-05 14:48:27 +0200

    Change model, enable resize

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-function addModelViewer(element){
+function addModelViewer(element) {
 
     let modelViewer = document.createElement("model-viewer")
 
@@ -8,21 +8,22 @@ function addModelViewer(element){
     return modelViewer
 }
 
-function setModel(modelViewerElement, model){
-    modelViewerElement.setAttribute("src", "models/" + model + ".glb")
+function setModel(modelViewerElement, model) {
     
+    modelViewerElement.setAttribute("src", model)
+
     modelViewerElement.setAttribute("autoplay", "true")
 
     modelViewerElement.setAttribute("ar", true)
     modelViewerElement.setAttribute("ar-modes", "webxr quick-look scene-viewer")
-    modelViewerElement.setAttribute("ar-scale", "fixed")
+    /*modelViewerElement.setAttribute("ar-scale", "fixed")*/
 
     modelViewerElement.setAttribute("shadow-intensity", 2)
 
     modelViewerElement.setAttribute("auto-rotate", true)
     modelViewerElement.setAttribute("camera-controls", true)
-    
+
 }
 
 let viewer = addModelViewer(document.body)
-setModel(viewer, "Chair")
+setModel(viewer, "https://storage.googleapis.com/ahojtabi/models/LPP_scene_Color.glb")
