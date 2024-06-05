0df3c7184c396aff4bfaafd2e6949b74c558c6d2
Miroslav Kotalik <mikotalik@seznam.cz>
2024-06-05 14:27:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function addModelViewer(element){
+
+    let modelViewer = document.createElement("model-viewer")
+
+    element.appendChild(modelViewer)
+
+    return modelViewer
+}
+
+function setModel(modelViewerElement, model){
+    modelViewerElement.setAttribute("src", "models/" + model + ".glb")
+    
+    modelViewerElement.setAttribute("autoplay", "true")
+
+    modelViewerElement.setAttribute("ar", true)
+    modelViewerElement.setAttribute("ar-modes", "webxr quick-look scene-viewer")
+    modelViewerElement.setAttribute("ar-scale", "fixed")
+
+    modelViewerElement.setAttribute("shadow-intensity", 2)
+
+    modelViewerElement.setAttribute("auto-rotate", true)
+    modelViewerElement.setAttribute("camera-controls", true)
+    
+}
+
+let viewer = addModelViewer(document.body)
+setModel(viewer, "Chair")
